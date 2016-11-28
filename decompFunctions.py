7fbe9179cd3ecb2d0509f3a06ee11849a8c12596
JSestito <jsestito@gatech.edu>
2016-11-28 00:13:07 -0500

    Added Analyze Scans Masking
    Created way to mask the data when performing PCA
    Added support in decompFunctions for this.

--- decompFunctions.py
+++ decompFunctions.py
@@ -3,6 +3,7 @@ from sklearn import metrics
 from sklearn import decomposition 
 from sklearn.cluster import KMeans, DBSCAN,MiniBatchKMeans
 import numpy as np
+import numpy.ma as ma
 import matplotlib.pyplot as plt
 
 def preping(data, logscaling = False, feature_scaling = False, **kwargs):
@@ -50,7 +51,7 @@ def getKmeansClusters(data, nClusters,**kwargs):
         ax.axis('off')
     return pltData, k_means
     
-def doPCA(data, nComponents, xvals = [], xlabel = '',normalize = False, returnComponents = -1, **kwargs):
+def doPCA(data, nComponents, mask = [], xvals = [], xlabel = '',normalize = False, returnComponents = -1, **kwargs):
     '''
     Does probabilistic PCA on data.
     
@@ -67,34 +68,38 @@ def doPCA(data, nComponents, xvals = [], xlabel = '',normalize = False, returnCo
     projected data, eigenvectors, list of explained variance ratio, PCA estimator object.
     
     '''
-    
+
+    if mask != []:
+        mask_prep = preping(mask, logscaling = False, feature_scaling=False, feature_range=(0.1,1))
+        data = ma.masked_array(data,mask_prep)    
+
     if normalize == True:
         #Find the Mean
-        meanvect = np.mean(data, axis = 0)
+        meanvect = np.mean(data, axis = 1)
         #Normalize Data
-        data = np.subtract(data,meanvect)
+        data = np.subtract(data.transpose(),meanvect).transpose()
         
     if returnComponents < 0:
         returnComponents = nComponents
     
-    print data[0,0]
     
     pca = decomposition.PCA(whiten = False, n_components = returnComponents)
     eigenVals = pca.fit_transform(data)
+                                          
     comps = pca.components_
     size = int(np.sqrt(comps.shape[-1]))
     pltData = comps.reshape(-1,size,size)
     varRatio = pca.explained_variance_ratio_
     
+    if mask != []:
+        pltData = ma.masked_array(pltData,mask)
+    
     if normalize == True:
-        meandata = meanvect.reshape(-1,size,size)
         fig,ax = plt.subplots()
-        cax = ax.imshow(meandata[0,:,:])
-        cbar = fig.colorbar(cax)
-        cbar.set_label('Intensity (Out of 2^16)')
-        ax.set_xlabel('x position')
-        ax.set_ylabel('y position')
         ax.set_title('Mean')
+        ax.plot(xvals,meanvect,marker = 'o', markerfacecolor='r')
+        ax.set_xlabel('$2\\theta$ (deg.)')
+        ax.set_ylabel('(Intensity) $ (arb. units)')
     
     fig, axes = plt.subplots(nComponents/2,2+nComponents%2, figsize = (12,12))
     #ig.subplots_adjust(wspace=0.1, hspace=0.1,bottom = 0., top=0.5)
@@ -149,14 +154,17 @@ def doNMF(data, nComponents,  xvals = [], xlabel='', **kwargs):
     size = int(np.sqrt(comps.shape[-1]))
     pltData = comps.reshape(-1,size,size)
     fig, axes = plt.subplots(nComponents/2,2+nComponents%2, figsize = (12,12))
-    fig.subplots_adjust(wspace=0.1, hspace=0.01,bottom = 0., top=0.5)
+    #fig.subplots_adjust(wspace=0.1, hspace=0.01,bottom = 0., top=0.5)
 #     fig.subplots_adjust(left=-0.01, bottom=-0.01, right=0.01, top=0.01,
 #                     wspace=0.01, hspace=0.01)
     for ax, imp, nComp in zip(axes.flat, pltData, np.arange(nComponents)):
-        ax.imshow(imp, **kwargs)
+        im = ax.imshow(imp, **kwargs)
         label = 'Component = %d' %(nComp+1)
         ax.axis('off')
         ax.set_title(label)
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    cbar = fig.colorbar(im, cax=cbar_ax)
     plt.show()
     fig, axes = plt.subplots(nComponents/2,2+nComponents%2, sharex = True, sharey = False, figsize = (12,12))
     fig.subplots_adjust(hspace=0.3)
@@ -202,3 +210,91 @@ def doICA(data, nComponents, xvals = [], xlabel = '', **kwargs):
         ax.set_xlabel(xlabel)
         
     return pltData, eigenVals, ica
+
+
+
+def doPCA_Mask(data, nComponents, mask3D, xvals = [], xlabel = '',normalize = True, returnComponents = -1, **kwargs):
+    '''
+    Does probabilistic PCA on data.
+    
+    Parameters:
+    -----------
+    data: output of preping function or ndarray with shape = (n_samples, n_features)
+    nComponents: # of principal axes to use.
+    xvals: Values to use for plotting the eigenvectors. Default is empty list.
+    xlabel: label for the x-axis. Default is ''.
+    **kwargs: keyword arguments passed to matplotlib ax.imshow() function.
+    
+    Returns:
+    --------
+    projected data, eigenvectors, list of explained variance ratio, PCA estimator object.
+    
+    '''
+
+    mask = preping(mask3D, logscaling = False, feature_scaling=False, feature_range=(0.1,1))
+    
+    
+    data_mask = np.zeros((mask.shape[0],int(np.sum(mask[0,:]))))
+    
+    j = 0
+    for i in range(0,mask.shape[1]):
+        if mask[0,i] == 0:
+            data_mask[:,j] = data[:,i]
+            j = j+1
+    
+    
+    #Find the Mean
+    meanvect = np.mean(data_mask, axis = 1)
+    #Normalize Data
+    data_mask = np.subtract(data_mask.transpose(),meanvect).transpose()
+        
+    if returnComponents < 0:
+        returnComponents = nComponents
+    
+    
+    pca = decomposition.PCA(whiten = False, n_components = returnComponents)
+    eigenVals = pca.fit_transform(data_mask)
+                                          
+    comps = pca.components_
+    varRatio = pca.explained_variance_ratio_
+    
+    comps_mask = np.zeros((comps.shape[0],mask.shape[1]))
+    j = 0
+    for i in range(0,mask.shape[1]):
+        if mask[0,i] == 0:
+            comps_mask[:,i] = comps[:,j]
+            j = j + 1
+   
+   
+    
+    fig,ax = plt.subplots()
+    ax.set_title('Mean')
+    ax.plot(xvals,meanvect,marker = 'o', markerfacecolor='r')
+    ax.set_xlabel('$2\\theta$ (deg.)')
+    ax.set_ylabel('(Intensity) $ (arb. units)')
+
+    size = int(np.sqrt(comps_mask.shape[-1]))
+    pltData = comps_mask.reshape(-1,size,size)
+    
+    
+    fig, axes = plt.subplots(nComponents/2,2+nComponents%2, figsize = (12,12))
+    for ax, imp, nComp in zip(axes.flat, pltData, np.arange(nComponents)):
+        im = ax.imshow(ma.masked_array(imp,mask3D[0,:,:], **kwargs))
+        label = 'Component = %d' %(nComp+1)
+        ax.axis('off')
+        ax.set_title(label)
+        
+      
+    fig, axes = plt.subplots(nComponents/2,2+nComponents%2, figsize = (12,12),sharex = True, sharey = False)
+    fig.subplots_adjust(hspace=0.3)
+    for ax, nComp, ratio in zip(axes.flat, np.arange(nComponents), varRatio):
+        if len(xvals) is not 0:
+            ax.plot(xvals[:], eigenVals[:,nComp],marker='o',markerfacecolor='r')
+        else:
+            ax.plot(eigenVals[:,nComp],marker='o',markerfacecolor='r')
+            
+        label = 'Component = %d, Exp. Var. Ratio = %2.3f  ' %(nComp+1, ratio)
+        ax.set_title(label)
+        ax.set_xlabel(xlabel)
+        
+    return np.array(comps), np.array(eigenVals), np.array(varRatio), pca
